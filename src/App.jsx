fe469d46d0f15f8bbaed2c49f263067aa5dbf7a1
Hari Prasath <prasathhari256@gmail.com>
2025-11-21 10:40:14 +0530

    Show the list of projects on sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -20,18 +20,18 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectsState((prevState) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random()
+        id: projectId
       }
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       }
     })
   }
-
-  console.log(projectsState);
 
   let content;
 
@@ -43,7 +43,7 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={handleSelectProject} />
+      <ProjectSidebar onStartAddProject={handleSelectProject} projects={projectsState.projects} />
       {content}
     </main>
   );
